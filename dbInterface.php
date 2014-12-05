a4b11b74d8f78cc673f01cd543d7f844846e4d7a
Stuart John Watson <ripppony@gmail.com>
2014-12-05 03:41:45 +0000

    Solve issue

--- dbInterface.php
+++ dbInterface.php
@@ -1,6 +1,6 @@
 <?php
     if ($_SERVER['REQUEST_METHOD'] === 'GET'){ #Get a card from the database
-        $conn = pg_connect("host=/var/run/postgresql/ dbname=ripp_");
+        $conn = pg_connect("host=/var/run/postgresql/ dbname=ripp_ user=ripp_");
         if(!$conn)
         {
             die ("Error connecting to PostGresSQL: " . pg_last_error());
@@ -13,6 +13,27 @@
         $id = pg_escape_string($_GET["id"]);
         $query = "SELECT * FROM tsssff_savedcards WHERE cardid = $id";
         $result = pg_query($query) or die('Query failed: ' . pg_last_error());
-        print json_encode($result);
+        $row = pg_fetch_row($result);
+        
+        print json_encode($row);
+    } else if ($_SERVER['REQUEST_METHOD'] === 'POST'){ #Save a card to the database
+        $conn = pg_connect("host=/var/run/postgresql/ dbname=ripp_ user=ripp_");
+        if(!$conn)
+        {
+            die ("Error connecting to PostGresSQL: " . pg_last_error());
+        }
+        $classes = pg_escape_string($_POST["classes"]);
+        $name = pg_escape_string($_POST["name"]);
+        $attr = pg_escape_string($_POST["attr"]);
+        $effect = pg_escape_string($_POST["effect"]);
+        $flavour = pg_escape_string($_POST["flavour"]);
+        $image = pg_escape_string($_POST["image"]);
+        
+        $query = "INSERT INTO tsssff_savedcards VALUES (
+            default,E'$classes',E'$name',E'$attr',E'$effect',E'$flavour',E'$image'
+        );";
+        
+        pg_query($query) or die('Query failed: ' . pg_last_error());
+        var_dump($row);
     }
 ?>
