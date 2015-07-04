17236745bb6f109d89351fe98737b3d156da9109
Latent Logic <lat.logic@gmail.com>
2015-07-03 18:53:54 -0700

    Don't throw warning on localhost images.
    PHP backend will still deny exporting, but this allows the frontend
    to be used for the new standalone backend being developed.

--- maker.js
+++ maker.js
@@ -485,7 +485,8 @@ function cardSetup(){
     $("#image").change(function(){
         var image_sites = [/^https?:\/\/i\.imgur\.com\//,
                            /^https?:\/\/img\.booru\.org\/secretshipfic\//,
-                           /^https?:\/\/derpicdn.net\//],
+                           /^https?:\/\/derpicdn.net\//,
+                           /^https?:\/\/(localhost|127\.[\d.]+|\[[0:]+1\])(:\d+)?\//],
             html_sites = [/^https?:\/\/(www\.)?imgur\.com\//,
                           /^https?:\/\/secretshipfic\.booru\.org\//,
                           /^https?:\/\/(www\.)?derpiboo\.ru\//],
